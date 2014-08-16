63389c8183fb687a8923789846bcbccad7363bff
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2014-08-16 20:35:09 +0300

    Use hostname() to find the server

--- lib/ex_canvas_client.ex
+++ lib/ex_canvas_client.ex
@@ -4,7 +4,10 @@ defmodule ExCanvasClient do
   def send(msg) do
     Node.start(:client, :shortnames)
     Node.set_cookie(:ex_canvas_cookie)
-    server = :server@hal9013
+    # :server@hostname
+    server = List.to_atom('server@' ++ case :inet.gethostname do
+      {:ok, hostname} -> hostname
+    end)
     IO.inspect(Node.ping(server))  # optional
     send({:ex_canvas_say, server}, {:data, [x: 10, y: 30, text: msg]})
   end
