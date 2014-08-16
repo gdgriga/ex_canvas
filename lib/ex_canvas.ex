1955b50563efe601f4cfe5a1d3dc9c9c787efbc0
Dimitry Solovyov <inbox@dimitry.so>
2014-08-16 16:13:25 +0300

    Getting there

--- lib/ex_canvas.ex
+++ lib/ex_canvas.ex
@@ -14,10 +14,35 @@ defmodule ExCanvas do
     |> send_file(200, "priv/index.html")
   end
 
+  get "/events" do
+    conn
+    |> put_resp_content_type("text/event-stream")
+    |> send_chunked(200)
+    |> send_events
+  end
+
+  defp send_events(conn) do
+    pid = spawn(fn -> canvas_loop(conn) end)
+    send(pid, {:data, "-1hello"})
+    send(pid, {:data, "-2hello"})
+    send(pid, {:data, "-3hello"})
+    conn
+  end
+
+  defp canvas_loop(conn) do
+    receive do
+      {:data, data} ->
+        case chunk(conn, ["data: ", data, "\n\n"]) do
+          {:ok, conn} -> canvas_loop(conn)
+          _ -> debug("failed to send chunk: " <> data)
+        end
+    end
+  end
+
   match _ do
     send_resp(conn, 404, "not found")
   end
 end
 
-info "Running on http://localhost:4000"
+info("Running on http://localhost:4000")
 Plug.Adapters.Cowboy.http ExCanvas, []
--- priv/static/ex_canvas.js
+++ priv/static/ex_canvas.js
@@ -13,4 +13,11 @@
     ctx.font = '2rem sans-serif';
     ctx.fillText('Hello world', 50, 50);
 
+    var evt = new EventSource('/events');
+    evt.onmessage = function(e) {
+        var x = Math.random() * 200;
+        var y = Math.random() * 200;
+        ctx.fillText(e.data, x, y);
+    };
+
 }());
