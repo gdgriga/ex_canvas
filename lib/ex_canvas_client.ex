de1e8d0c09cff10ac09f9e1b0a7c0699def93a68
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2014-08-25 16:08:14 +0300

    Print Erlang nodes

--- lib/ex_canvas_client.ex
+++ lib/ex_canvas_client.ex
@@ -1,12 +1,17 @@
 defmodule ExCanvasClient do
   def send(msg) do
+    IO.inspect(node())
     Node.start(:client, :shortnames)
     Node.set_cookie(:ex_canvas_cookie)
     # :server@hostname
     server = List.to_atom('server@' ++ case :inet.gethostname do
       {:ok, hostname} -> hostname
     end)
+    IO.inspect(node())
+    IO.inspect(Node.list())
     IO.inspect(Node.ping(server))  # optional
+    IO.inspect(Node.list())
+    IO.inspect(Node.list(:known))
     send({:ex_canvas_say, server}, {:data, [x: 10, y: 30, text: msg]})
   end
 end
