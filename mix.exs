c5d9445fbb952ffc60b05140d1970ec318ae583d
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2014-09-08 21:03:24 +0300

    Update Elixir and libs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExCanvas.Mixfile do
   def project do
     [app: :ex_canvas,
      version: "0.0.1",
-     elixir: ">= 0.15.1",
+     elixir: ">= 1.0.0-rc2",
      deps: deps,
      aliases: aliases]
   end
@@ -15,8 +15,8 @@ defmodule ExCanvas.Mixfile do
 
   defp deps do
     [{:cowboy, "~> 1.0.0"},
-     {:plug, "~> 0.5.3"},
-     {:jsx, "~> 2.0.4"}]
+     {:plug, "~> 0.7.0"},
+     {:jsx, "~> 2.1.1"}]
   end
 
   defp aliases do
